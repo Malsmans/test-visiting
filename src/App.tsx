031b214be90adf2bb62024f6d45ed87195b2ba98
Malsmans <143906567+Malsmans@users.noreply.github.com>
2025-09-28 17:00:14 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import CountryDetails from './pages/CountryDetails';
 import Search from './pages/Search';
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <div className="min-h-screen luxury-african-bg">
         <Header />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
